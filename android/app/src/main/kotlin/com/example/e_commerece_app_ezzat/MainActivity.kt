8d17090d75e9a9b4a5bf6cacd608ea9aedc8816b
ezzat <coezz@yahoo.com>
2021-02-22 11:10:13 +0200

    version 6 finial signed v3

--- android/app/src/main/kotlin/com/example/e_commerece_app_ezzat/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.e_commerece_app_ezzat
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/programmerezzat2021/e_commerece_app_ezzat/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.programmerezzat2021.e_commerece_app_ezzat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
